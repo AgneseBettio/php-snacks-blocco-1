5020bb19b8cb78178b5dcdc3bbadf93b8158f080
AgneseBettio <bettio.agnese@gmail.com>
2021-05-31 18:10:18 +0200

    ciclo for per esercizio personale

--- snack_4.php
+++ snack_4.php
@@ -3,18 +3,33 @@ Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà co
 
 <?php
 
-$newArray = [];
+//ciclo while
 
-while (count($newArray) < 15) {
+$whileArray = [];
+
+while (count($whileArray) < 15) {
    
     $rndNum = rand(1, 100);
-    if (!in_array($rndNum, $newArray)) {
-        $newArray[] = $rndNum;
+    if (!in_array($rndNum, $whileArray)) {
+        $whileArray[] = $rndNum;
     }
 }
 
 
-var_dump($newArray);
+var_dump($whileArray);
 
+
+//ciclo for
+
+$forArray = [];
+for ($i = 0; $i < 15; $i++) {
+    $rndNum = rand(1, 100);
+    if (!in_array($rndNum, $forArray)) {
+        $forArray[] = $rndNum;
+    }
+
+}
+
+var_dump($forArray);
 
 ?>
