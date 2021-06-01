5bb0f180a302d6d0252cb0e4d3648b41e2cf989d
AgneseBettio <bettio.agnese@gmail.com>
2021-06-01 10:03:09 +0200

    sistemato

--- snack_1.php
+++ snack_1.php
@@ -46,3 +46,32 @@ var_dump($dailyGames);
 
 
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <?php
+        for($i = 0; $i < count($dailyGames); $i++){
+            $match = $dailyGames[$i];
+            $testoFinale = '';
+            //variabili nomi squadre
+            $nomiSquadre = $match['homeTeam'] . "-" . $match['visitingTeam'];
+            //variabili risultati
+            $scoreSquadre = $match['homeScore'] . "-" . $match['visitingScore'];
+            $testoFinale = $nomiSquadre . "|" . $scoreSquadre;
+        ?>
+            <li>
+                <?php echo $testoFinale ?>
+            </li>
+        <?php
+        }
+        ?>
+    </ul>
+</body>
+</html>
